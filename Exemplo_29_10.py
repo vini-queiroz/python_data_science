879c00567c95303578b3ef8c42034c0cccb49545
vini-queiroz <134077851+vini-queiroz@users.noreply.github.com>
2025-11-03 14:26:23 -0300

    Delete Exemplo_29_10.py

--- Exemplo_29_10.py
+++
@@ -1,9 +0,0 @@
-n = int(input('Digite um número para saber se é igual a 10:'))
-
-if 10 == n:
-    print('É igual a 10')
-elif 10 > n:
-    print('É menor que 10')
-else:
-    print('É maior que 10')
-        
